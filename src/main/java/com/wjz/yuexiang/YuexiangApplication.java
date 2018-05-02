458b9a462840462e997da4d35fb73ac3157155fa
wjz <279660577@qq.com>
2018-05-02 09:45:18 +0800

    created project

---
+++ src/main/java/com/wjz/yuexiang/YuexiangApplication.java
@@ -0,0 +1,12 @@
+package com.wjz.yuexiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuexiangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuexiangApplication.class, args);
+	}
+}
